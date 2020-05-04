758a5f46cdb288565d6fc1078e3e443255a0b037
Thore <hildebrandtthore@gmail.com>
2020-05-04 15:22:27 +1200

    added example form pinocchio paper

--- example_circuit_from_paper.py
+++ example_circuit_from_paper.py
@@ -30,6 +30,34 @@ def vanishing_poly(S):
     return p
 
 
+def use_Pinocchio_paper_example(b0, b1, b2, b3):
+    a = np.array([Fp(x) for x in [1, b0, b1, b2, b3]])
+    L0 = np.array([Fp(x) for x in [2, 0, 0, 0, 0]])
+    L1 = np.array([Fp(x) for x in [0, 1, 0, 0, 0]])
+    L2 = np.array([Fp(x) for x in [0, 0, 1, 0, 0]])
+    L3 = np.array([Fp(x) for x in [0, 0, 0, 1, 0]])
+    L4 = np.array([Fp(x) for x in [0, 0, 0, 0, 1]])
+    L = np.array([L0, L1, L2, L3, L4])
+
+    R0 = np.array([Fp(x) for x in [1, 0, 0, 0, 0]])
+    R1 = np.array([Fp(x) for x in [0, 1, 0, 0, 0]])
+    R2 = np.array([Fp(x) for x in [0, 0, 1, 0, 0]])
+    R3 = np.array([Fp(x) for x in [0, 0, 0, 1, 0]])
+    R4 = np.array([Fp(x) for x in [0, 0, 0, 0, 1]])
+    R = np.array([R0, R1, R2, R3, R4])
+
+    O0 = np.array([Fp(x) for x in [0, 1, 2, 4, 8]])
+    O1 = np.array([Fp(x) for x in [0, 1, 0, 0, 0]])
+    O2 = np.array([Fp(x) for x in [0, 0, 1, 0, 0]])
+    O3 = np.array([Fp(x) for x in [0, 0, 0, 1, 0]])
+    O4 = np.array([Fp(x) for x in [0, 0, 0, 0, 1]])
+    O_ = np.array([O0, O1, O2, O3, O4])
+
+    print("La * Ra: = ", L.dot(a) * R.dot(a))
+    print("Oa = ", O_.dot(a))
+    return L, R, O_, a
+
+
 def use_paper_example(wire_a, wire_b, wire_NAND):
     a = np.array([Fp(x) for x in [1, wire_a, wire_b, wire_NAND]])
     U1 = np.array([Fp(x) for x in [-1, 2, 0, 0]])
@@ -298,6 +326,7 @@ def testingProof():
 
     n_stmt = 1
     L, R, O_, a = use_paper_example(1, 1, 0)
+    # L, R, O_, a = use_Pinocchio_paper_example(0, 1, 0, 0, 0)
 
     (m, n) = L.shape
     a_stmt = a[:n_stmt]
